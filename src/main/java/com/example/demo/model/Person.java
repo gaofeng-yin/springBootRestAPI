b4f56979f5c704838c8ece6eb00edc74ff068f35
gaofeng <gaofengyin@hotmail.com>
2021-03-22 15:24:09 +0000

    Person properties, constructor and getter

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,5 +1,24 @@
 package com.example.demo.model;
 
-public class Person {
+import java.util.UUID;
 
+public class Person {
+	
+	private final UUID id;
+	private final String name;
+	
+	public Person(UUID id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	
 }
