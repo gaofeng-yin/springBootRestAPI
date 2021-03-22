097746c415f879c79074e37a2c29888df464959f
gaofeng <gaofengyin@hotmail.com>
2021-03-22 17:34:23 +0000

    done

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -2,6 +2,8 @@ package com.example.demo.model;
 
 import java.util.UUID;
 
+import org.springframework.lang.NonNull;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Person {
